e99540df50b0126897620bfeb2489178c0f86419
Sebastien von Roth <svonroth@gmail.com>
2016-08-24 18:08:43 +0200

    Init

---
+++ pok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pok
+//
+//  Created by Sébastien Von Roth on 23.08.16.
+//  Copyright © 2016 Sébastien Von Roth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
